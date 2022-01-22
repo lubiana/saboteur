b0c5995b9d0ab6acc9505a24b0d50226c4aa4ff1
xkizilx <xkizilx@gmail.com>
2022-01-22 17:42:17 +0200

    upgrade framework

--- src/phases/play/moves/discardCard.ts
+++ src/phases/play/moves/discardCard.ts
@@ -4,21 +4,22 @@ import {INVALID_MOVE} from "boardgame.io/core";
 import {HandCard} from "../../../types/Cards";
 
 const discardCard = (G: GameState, ctx: Ctx) => {
-    const currentPlayer = Number(ctx.currentPlayer)
-    const selectedCard = G.players[currentPlayer].selectedCard
-    if (selectedCard === undefined) {
-        return INVALID_MOVE
-    }
+  const currentPlayer = Number(ctx.currentPlayer)
+  const selectedCard = G.players[currentPlayer].selectedCard
+  if (selectedCard) {
 
     const card: HandCard[] = G.players[currentPlayer].hand.splice(selectedCard, 1)
     G.players[currentPlayer].selectedCard = undefined
     G.discardPile.push(...card)
-    const playerCard = G.drawPile.pop()
 
-    if (playerCard !== undefined) {
-        G.players[currentPlayer].hand.push(playerCard)
+    const playerCard = G.drawPile.pop()
+    if (playerCard) {
+      G.players[currentPlayer].hand.push(playerCard)
     }
     ctx.events?.endTurn()
+  } else {
+    return INVALID_MOVE
+  }
 }
 
 export default discardCard
