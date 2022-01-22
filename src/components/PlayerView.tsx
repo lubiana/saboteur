536e21ee40c97ba9b23313b4b089fbfd8231114d
xkizilx <xkizilx@gmail.com>
2022-01-22 17:57:12 +0200

    minor correction

--- src/components/PlayerView.tsx
+++ src/components/PlayerView.tsx
@@ -9,19 +9,21 @@ interface RoleCardPickerProps {
   moves: any
 }
 
-const PlayerView: React.FC<RoleCardPickerProps> = ({ player, moves }) => (
+const PlayerView: React.FC<RoleCardPickerProps> = ({player, moves}) => (
   <div>
-    <hr />
+    <hr/>
     <Grid container spacing={5} justifyContent="left">
       <Grid item key="roleCard">
-        <SaboteurCard card={{ type: "Role", role: player.role || "Digger" }} />
+        {player.role && (
+          <SaboteurCard card={{type: "Role", role: player.role}} elevation={1}/>
+        )}
       </Grid>
       {player.hand.map((card: HandCard, index: number) => (
         <Grid item key={index}>
           <SaboteurCard
             card={card}
             onClick={() => moves.selectCard(index)}
-            elevation={player.selectedCard === index ? 5 : 1} />
+            elevation={player.selectedCard === index ? 5 : 1}/>
         </Grid>
       ))}
     </Grid>
